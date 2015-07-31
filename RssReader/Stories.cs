239eee121dcc63048d560792e575c20966a6f7b7
yatinshelke <yatin.s.shelke@gmail.com>
2015-07-31 18:37:13 -0400

    Added more content for unit tests for Stories constructor

--- RssReader/Stories.cs
+++ RssReader/Stories.cs
@@ -15,12 +15,33 @@ namespace RssReader
             private string feedUri;
             public Stories(string feedIdentifier)
             {
-                var knownFeeds = ConfigurationManager.GetSection("feeds") as NameValueCollection;
-                if (knownFeeds != null)
+                Uri uriResult;
+                bool feedIdentifierIsUri = false;
+                if (Uri.TryCreate(feedIdentifier, UriKind.Absolute, out uriResult)) 
                 {
-                    feedUri = knownFeeds.Get(feedIdentifier);
-                    if (feedUri == null) {
-                        feedUri = feedIdentifier;
+                    if (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps)
+                    {
+                        feedIdentifierIsUri = true;
+                    }
+                }
+                if (feedIdentifierIsUri) 
+                {
+                    feedUri = feedIdentifier;
+                } 
+                else 
+                {
+                    var knownFeeds = ConfigurationManager.GetSection("feeds") as NameValueCollection;
+                    if (knownFeeds != null)
+                    {
+                        feedUri = knownFeeds.Get(feedIdentifier);
+                        if (feedUri == null)
+                        {
+                            throw new Exception("Invalid RSS feed identifier");
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("App does not know the URI for the RSS feed for RSS feed identifier: " + feedIdentifier);
                     }
                 }
             }
--- UnitTestRssReaderModel/UnitTestStories.cs
+++ UnitTestRssReaderModel/UnitTestStories.cs
@@ -10,10 +10,61 @@ namespace UnitTestRssReaderModel
         [TestMethod]
         public void TestStoriesConstructor()
         {
-            Stories storiesBBC = new Stories("BBC");
-            Assert.IsNotNull(storiesBBC);
-            Stories storiesCNN = new Stories("CNN");
-            Assert.IsNotNull(storiesCNN);
+            string[] validTestFeedNames = new string[] {"BBC", "CNN"};
+
+            for (int i = 0; i < validTestFeedNames.Length; i++)
+            {
+                Stories stories = new Stories(validTestFeedNames[i]);
+                Assert.IsNotNull(stories);
+            }
+
+            string[] invalidTestFeedNames = new string[] { "XYZ", "FOO", "BAR" };
+
+            for (int i = 0; i < invalidTestFeedNames.Length; i++)
+            {
+                bool failedOnInvalidRSSIdentifier = false;
+                try
+                {
+                    Stories stories = new Stories(invalidTestFeedNames[i]);
+                }
+                catch (Exception e)
+                {
+                    failedOnInvalidRSSIdentifier = true;
+                }
+                Assert.IsTrue(failedOnInvalidRSSIdentifier);
+            }
+
+            // these just have to be valid http or https URI's, not necessarily RSS feeds
+            string[] validUris = new string[] {
+                "http://www.cnn.com",
+                "https://gmail.com",
+                "http://feeds.abcnews.com/abcnews/internationalheadlines",
+                "http://feeds.bbci.co.uk/news/rss.xml",
+            };
+            for (int i = 0; i < validUris.Length; i++)
+            {
+                Stories stories = new Stories(validUris[i]);
+                Assert.IsNotNull(stories);
+            }
+            string[] invalidUris = new string[] {
+                "http:///www.cnn.com",
+                "https:/gmail.com",
+                "ftp://feeds.abcnews.com/abcnews/internationalheadlines",
+                "file://feeds.bbci.co.uk/news/rss.xml",
+            };
+            for (int i = 0; i < invalidUris.Length; i++)
+            {
+                bool failedOnInvalidRSSIdentifier = false;
+                try
+                {
+                    Stories stories = new Stories(invalidUris[i]);
+                }
+                catch (Exception e)
+                {
+                    failedOnInvalidRSSIdentifier = true;
+                }
+                Assert.IsTrue(failedOnInvalidRSSIdentifier);
+            }
         }
     }
 }
