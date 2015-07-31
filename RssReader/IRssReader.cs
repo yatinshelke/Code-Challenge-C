dbc12d0163a7917c9ef80016ba27ffae720668d9
yatinshelke <yatin.s.shelke@gmail.com>
2015-07-30 23:34:57 -0400

    Comments on IRSSReader

--- RssReader/IRssReader.cs
+++ RssReader/IRssReader.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace RssReader
 {
     // Ready for DI.
+    // *** Either this interface is not needed or RssReader should implement this interface
+    // *** An interface makes sense if there will be multiple implementations of the interface
+    // *** or if there is intention of additional implementations
+    // *** E.g. one implementation uses algorithm X to get top stories and another uses algorithm Y
     public interface IRssReader
     {
         void LoadStories();
